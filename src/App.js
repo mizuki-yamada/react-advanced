d2a3ee74a769102d1c54779512a2fb2bcff91ced
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-07-01 20:18:35 +0900

    cssモジュール学習のための雛形を作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import './App.css';
-import { useState } from 'react';
-import { ChildArea } from './components/ChildArea';
+import { InlineStyle } from './components/InlineStyle';
 
 function App() {
-  console.log("App");
-  const [inputText, setInputText] = useState("");
-  const [isShowing, setIsShowing] = useState(false);
-
-  const changeIsShowingFlag = () => {
-    return setIsShowing(!isShowing)
-  }
-
-  const onClickClose = () => {
-     return setIsShowing(false)
-  }
   
   return (
     <div className="App">
-      <input
-        value={inputText}
-        onChange={(event) => {
-          setInputText(event.target.value);
-        }}
-      />
-      <br></br>
-      inputされた文字列：{inputText}
-      <br></br>
-      <button onClick={changeIsShowingFlag}>表示</button>
-      <br></br>
-      <ChildArea open={isShowing} onClose={onClickClose}></ChildArea>
+     <InlineStyle></InlineStyle>
     </div>
   );
 }
