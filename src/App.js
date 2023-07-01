1e236fe0848d34cf1889728e04af515ea9cf7e96
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-07-01 21:00:22 +0900

    cssモジュールを学習

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/CssModules';
 
 function App() {
   
   return (
     <div className="App">
-     <InlineStyle></InlineStyle>
+      <InlineStyle></InlineStyle>
+      <CssModules></CssModules>
     </div>
   );
 }
